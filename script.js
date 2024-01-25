dcea8bc708aef52b51e1f31887c022ecf1703de5
shashank <shashank770@gmail.com>
2024-01-25 19:08:50 +0530

    moving cursor animation

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+window.onload=function(){
+  new Typed('#typed',{
+      strings:["Full Stack Developer","Coffee Lover","Batman🦇"],
+      typeSpeed:100,
+      backSpeed:50,
+      backDelay:2000,
+      loop:true,
+      smartBackspace:true,
+      callback:function(){}
+  })
+
+
+  preloaderFadeOutTime = 500;
+      function hidePreloader() {
+      $('.spinner-wrapper').fadeOut(preloaderFadeOutTime);
+      }
+      hidePreloader();
+
+
+}
+
 function toggleMenu() {
   const menu = document.querySelector(".menu-links");
   const icon = document.querySelector(".hamburger-icon");
